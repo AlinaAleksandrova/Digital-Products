7f16af620e6838ed7a7aa607d95a93fe6d17c817
Alina <aleksandrovaalina4@gmail.com>
2024-01-27 22:49:03 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#slider").owlCarousel({
+        items: 1
+    });
+    $('#scroll').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $("#firth-section").offset().top
+        }, 500);
+    });
+    var $menu = $(".section-menu");
+    var $menuOffsetTop = $menu.offset().top;
+    var $menuHeight = $menu.outerHeight();
+    var $menuParent = $menu.parent();
+    var $menuParentPaddingTop = parseFloat($menuParent.css("padding-top"));
+
+    checkWidth();
+
+    function checkWidth() {
+        // for mobile - you need change 1920 to 992
+        if (window.matchMedia("(max-width: 1920px)").matches) {
+            if ($menu.length !== 0) {
+                $(window).scroll(onScroll);
+            }
+        }
+    }
+
+    function onScroll() {
+        if ($(window).scrollTop() > $menuOffsetTop) {
+            $menu.addClass("fixed");
+            $menuParent.css({ "padding-top": $menuParentPaddingTop + $menuHeight });
+        } else {
+            $menu.removeClass("fixed");
+            $menuParent.css({ "padding-top": $menuParentPaddingTop });
+        }
+    }
+});
